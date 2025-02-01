28047cb7c001291fcac5fba9dcb7dfbb7098ed1d
Welton <weltonrdesigner@gmail.com>
2025-02-01 19:28:14 -0300

    estudei o loop while

--- Condicionais/condicionais.py
+++ Condicionais/condicionais.py
@@ -1,14 +1,17 @@
+#Decisões simples(apenas if), compostas(if e else) e encadeadas(if, elife e else)
 
 
-nota1 = float(input("Diite a nota de Portugês! "))
-nota2 = float(input("Diite a nota de Matemática! "))
-nota3 = float(input("Diite a nota de História! " ))
-nota4 = float(input("Diite a nota de Geografia! "))
-nota5 = float(input("Diite a nota de Biologia! "))
+nota1 = float(input("Digite a nota de Portugês! "))
+nota2 = float(input("Digite a nota de Matemática! "))
+nota3 = float(input("Digite a nota de História! " ))
+nota4 = float(input("Digite a nota de Geografia! "))
+nota5 = float(input("Digite a nota de Biologia! "))
 
 media = (nota1 + nota2 + nota3 + nota4 + nota5) /5
 
 if media >= 7:
     print("Aprovado! Sua média é: ", media)
+elif media >=6:
+    print("Recuperação! Sua média é: ", media)
 else:
     print("Reprovado! Sua média é: ", media)
---
+++ Loops/while.py
@@ -0,0 +1,6 @@
+numero = int(input("Digite um número! "))
+
+while numero <= 50:
+    print(" " + str(numero))
+    numero +=1
+print("\nFim do loop")
