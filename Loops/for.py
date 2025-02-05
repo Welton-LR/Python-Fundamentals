c935f6d58744696f3e68040f4b58c46a6682141e
Welton <weltonrdesigner@gmail.com>
2025-02-04 21:04:51 -0300

    estudei o loop for

---
+++ Loops/for.py
@@ -0,0 +1,13 @@
+# Primeiro exemplo:
+# Para cada valor(time) dentro da variável times,
+# execulte uma vez a função
+
+times = ["Santos","Palmeiras","Botafogo","Flamengo"]
+for time in times:
+    print(time)
+    
+# Este usa a quantidade dentro de range()
+# onde começa em zero e vai até o 6 sem incluir o 7
+
+for numero in range(0,7):
+    print(numero, end=" ")
