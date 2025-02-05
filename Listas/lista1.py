bd5ade3d394b22fb9cf51fb4f3dd23400927f9f2
Welton <weltonrdesigner@gmail.com>
2025-02-04 21:13:53 -0300

    listas

---
+++ Listas/lista1.py
@@ -0,0 +1,8 @@
+# Listas preenchida estaticamente
+lista_estatica = ["user 02",True]
+
+# Lista preenchida dinamicamente
+lista_dinamica = [input("Digite o nome do usuário: "), bool(int(input("Está logado? ")))]
+
+# Lista vazia
+lista_vazia = []
